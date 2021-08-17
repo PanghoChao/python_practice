fdcc9e15a26fcfdba58b6783054ad0bc0bbd8553
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-08-17 16:03:46 +0900

    Add files via upload

---
+++ python workspace/operator/operator.py
@@ -0,0 +1,43 @@
+'''
+print(1+1) #2
+print(2**3) #2^3 8
+print(5%3) # 나머지 2
+print(5//3) # 몫만  1
+print(8/3) # 2.66666666666666
+'''
+# 'and' =='&'
+# 'or'=='|'
+
+
+# print (2 + 3 * 4) #14
+
+# print(abs(-5)) #5
+# print(pow(4,2)) #4^2 = 16
+# print(max(5,12)) # 12
+# print(min(5,12)) # 5
+# print(round(3.14)) # 3 반올림
+# print(round(4.99)) # 5 반올림
+
+# from math import *
+# print(floor(4.99)) # 내림
+# print(ceil(3.14)) # 올림 
+# print(sqrt(16)) # 제곱근
+
+# #랜덤함수
+
+# from random import*
+
+# print(random()) #0.0이상 1.0 미만의 임의의 값 생성
+
+# print(int(random()* 10)) #0~10미만의 임의의 값 
+# print(randrange(1,46)) # 1~46미만
+# print(randint(1,45)) # 1~45이하
+
+
+'''
+
+'''
+from random import*
+date = randint(4, 28)
+
+print("오프라인 스터디 모임 날짜는 매월 "+str(date) +"일로 선정되었습니다.")
